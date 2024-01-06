0fc5710acf245be4c4b9b7e977f28fcbcbbf98f5
Ragulja <106649240+Ragulja@users.noreply.github.com>
2024-01-06 17:42:43 +0530

    Add files via upload

---
+++ city.js
@@ -0,0 +1,46 @@
+//const button=document.getElementById("button")
+//event listener
+//button.addEventListener('click',showstatics)
+//function showstatics(
+
+    const button=document.querySelector('button')
+    //event listener
+    button.addEventListener('click',showststic)
+    function showststi(){
+        
+
+    }
+
+
+
+
+//objects 
+let item= {
+    color:'Red',
+    brand:'dell',
+    price:2300,
+}
+console.log(item.price)
+item= {  //mehtods in js
+    color:'Red',
+    brand:'dell',
+    price:2300,
+    deve(){
+        console.log('our are the developer')
+    }
+}
+item.deve()
+//loops
+let i=10
+for(i=1;i<=10;i++){
+    console.log(i)
+}
+i=10
+while(i>=1){
+    i--;
+    console.log(i)
+}
+
+
+
+
